d5f7679dcd4ee1f8974ba809a4d52975a653aff3
sikuwa <93775050+sikuwa@users.noreply.github.com>
2025-06-01 20:51:21 +0200

    Add files via upload

---
+++ js/mapdata.js
@@ -0,0 +1,222 @@
+var simplemaps_europemap_mapdata={
+  main_settings: {
+   //General settings
+    width: "responsive", //'700' or 'responsive'
+    background_color: "#0xFF",
+    background_transparent: "yes",
+    border_color: "#ccc",
+    popups: "detect",
+    state_description: "",
+    state_color: "#ededed",
+    state_hover_color: "#ccc",
+    state_url: "",
+    border_size: 1.5,
+    all_states_inactive: "no",
+    all_states_zoomable: "yes",
+    location_description: "",
+    location_color: "#88a4bc",
+    location_opacity: 0.8,
+    location_hover_opacity: 1,
+    location_url: "",
+    location_size: "2",
+    location_type: "square",
+    location_image_source: "frog.png",
+    location_border_color: "#FFFFFF",
+    location_border: 2,
+    location_hover_border: 2.5,
+    all_locations_inactive: "no",
+    all_locations_hidden: "no",
+    
+    //Label defaults
+    label_color: "#d5ddec",
+    label_hover_color: "#d5ddec",
+    label_size: 22,
+    label_font: "Raleway",
+    hide_labels: "no",
+    manual_zoom: "no",
+    back_image: "no",
+    arrow_color: "#cecece",
+    arrow_color_border: "#808080",
+    initial_back: "no",
+    initial_zoom: -1,
+    initial_zoom_solo: "no",
+    region_opacity: 1,
+    region_hover_opacity: 0.6,
+    zoom_out_incrementally: "yes",
+    zoom_percentage: 0.99,
+    zoom_time: 0.5,
+    
+    //Popup settings
+    popup_color: "white",
+    popup_opacity: 0.9,
+    popup_shadow: 1,
+    popup_corners: 5,
+    popup_font: '12px/1.5 "Raleway", "Helvetica Neue", Helvetica, Raleway, sans-serif',
+    popup_nocss: "no",
+    
+    //Advanced settings
+    div: "map",
+    auto_load: "yes",
+    url_new_tab: "no",
+    images_directory: "default",
+    fade_time: 0.1,
+    link_text: "View Website",
+    state_image_url: "",
+    state_image_position: "",
+    location_image_url: ""
+  },
+  state_specific: {
+    AL: {
+      name: "Albania"
+    },
+    AM: {
+      name: "Armenia",
+      hide: "yes"
+    },
+    AT: {
+      name: "AUSTRIA",
+      description: "",
+      color: "#bfbd80",
+      hover_color: "#5d7d37"
+    },
+    BA: {
+      name: "Bosnia and Herzegovina"
+    },
+    BE: {
+      name: "Belgium"
+    },
+    BG: {
+      name: "Bulgaria"
+    },
+    BY: {
+      name: "Belarus"
+    },
+    CH: {
+      name: "SWITZERLAND",
+      description: "",
+      color: "#bfbd80",
+      hover_color: "#5d7d37"
+    },
+    CY: {
+      name: "Cyprus",
+      hide: "yes"
+    },
+    CZ: {
+      name: "Czech Republic"
+    },
+    DE: {
+      name: "GERMANY",
+      description: "",
+      color: "#bfbd80",
+      hover_color: "#5d7d37"
+    },
+    DK: {
+      name: "Denmark"
+    },
+    EE: {
+      name: "Estonia"
+    },
+    ES: {
+      name: "Spain"
+    },
+    FI: {
+      name: "Finland"
+    },
+    FR: {
+      name: "France"
+    },
+    GB: {
+      name: "United Kingdom"
+    },
+    GE: {
+      name: "Georgia",
+      hide: "yes"
+    },
+    GR: {
+      name: "Greece"
+    },
+    HR: {
+      name: "Croatia"
+    },
+    HU: {
+      name: "Hungary"
+    },
+    IE: {
+      name: "Ireland"
+    },
+    IS: {
+      name: "Iceland",
+      hide: "yes"
+    },
+    IT: {
+      name: "Italy"
+    },
+    LT: {
+      name: "Lithuania"
+    },
+    LU: {
+      name: "Luxembourg"
+    },
+    LV: {
+      name: "Latvia"
+    },
+    MD: {
+      name: "Moldova"
+    },
+    ME: {
+      name: "Montenegro"
+    },
+    MK: {
+      name: "Macedonia"
+    },
+    NL: {
+      name: "Netherlands"
+    },
+    NO: {
+      name: "Norway"
+    },
+    PL: {
+      name: "Poland"
+    },
+    PT: {
+      name: "Portugal"
+    },
+    RO: {
+      name: "Romania"
+    },
+    RS: {
+      name: "Serbia"
+    },
+    SE: {
+      name: "Sweden"
+    },
+    SI: {
+      name: "SLOVENIA",
+      color: "#bfbd80",
+      description: "Slovenija",
+      hover_color: "#5d7d37"
+    },
+    SK: {
+      name: "Slovakia"
+    },
+    TR: {
+      name: "Turkey",
+      hide: "yes"
+    },
+    UA: {
+      name: "Ukraine"
+    }
+  },
+  locations: {
+    "0": {
+      lng: "14.505751",
+      name: "Ljubljana",
+      lat: "46.056946"
+    }
+  },
+  regions: {},
+  labels: {},
+  legend: {
+    entries: []
+  }
+};
